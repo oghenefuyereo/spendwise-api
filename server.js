99e7004e5f7aff0e99140e123a09f45f7770d9d1
oghenefuyereo <oghenefuyereo@gmail.com>
2025-08-10 11:25:43 -0400

    update

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
-require('dotenv').config(); // Load environment variables from .env file
+require("dotenv").config();
 
-const app = require('./app');
-const connectDB = require('./config/db');
+const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 3000;
 
 async function startServer() {
   try {
     await connectDB();
-    console.log('MongoDB connected');
+    console.log("MongoDB connected");
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   } catch (err) {
-    console.error('Failed to connect to MongoDB:', err);
-    process.exit(1); // Exit if DB connection fails
+    console.error("Failed to connect to MongoDB:", err);
+    process.exit(1);
   }
 }
 
